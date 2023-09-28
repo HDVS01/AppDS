e67f964f04e85edd656732d4f55509f8124247d4
gusls02 <A01721747@tec.mx>
2023-09-28 12:03:14 -0600

    crearSessionView

--- Proyecto/Proyect/Proyect/CreateSessionView.swift
+++ Proyecto/Proyect/Proyect/CreateSessionView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct CreateSessionView: View {
+    @State private var maxAmount = 10
+    @State private var sessionType = "School"
+    let amountPlayers = [10,20,30,40]
+    let sessions = ["School", "House"]
+    
     var body: some View {
-        Text("CREAR SESIÓN")
+        Text("Configure Session")
+            .bold()
+        Form{
+            Section {
+                Picker("Amount of players: ", selection: $maxAmount){
+                    ForEach(amountPlayers, id:\.self){
+                        Text("\($0)")
+                    }
+                }
+            }
+            Section {
+                Picker("Type of Scene: ", selection: $maxAmount){
+                    ForEach(sessions, id:\.self){
+                        Text($0)
+                    }
+                }
+            }
+            NavigationLink(destination: MainView()){
+                Text("Iniciar Sesion")
+            }
+        }
     }
 }
 
