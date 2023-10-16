fdedf67a6e7d2b6421735c17f249c7077933c33f
Juanster08 <88806110+Juanster08@users.noreply.github.com>
2023-10-16 12:43:01 -0600

    Update JoinSessionView.swift
    join session restored.

--- DiloenSenas/DiloenSenas/JoinSessionView.swift
+++ DiloenSenas/DiloenSenas/JoinSessionView.swift
@@ -6,115 +6,27 @@
 //
 
 import SwiftUI
-import UIKit
-import AVFoundation
 
 struct JoinSessionView: View {
-    @State private var showPINInput = false
-    @State private var showQRScanner = false
-    @State private var enteredPIN = "1234"
-    @State private var isSessionJoined = false
-    
     var body: some View {
         NavigationView {
             ZStack {
-                Color(red: 244/255, green: 240/255, blue: 236/255)
+                Image("fondosolo")
+                    .resizable()
+                    .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
-                VStack {
-                    HStack {
-                        Button(action: {
-                            self.showPINInput = true
-                            self.showQRScanner = false
-                        }) {
-                            Text("Ingresa PIN")
-                                .frame(width: 150, height: 50)
-                                .background(Color(red: 255/255, green: 87/255, blue: 50/255))
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .bold()
-                        }
-                        .padding()
-                        
-                        Button(action: {
-                            self.showPINInput = false
-                            self.showQRScanner = true
-                        }) {
-                            Text("Escanea QR")
-                                .frame(width: 150, height: 50)
-                                .background(Color(red: 255/255, green: 87/255, blue: 50/255))
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .bold()
-                        }
-                        .padding()
-                    }
-                    Spacer()
-                    if showPINInput {
-                        // Vista para ingresar el PIN
-                        TextField("Ingresa el PIN", text: $enteredPIN)
-                            .padding()
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        Button(action: {
-                            // Verificar el PIN y activar la navegación si es correcto
-                            if enteredPIN == "1234" { // Reemplaza "PIN_CORRECTO" con el valor correcto
-                                isSessionJoined = true
-                            }
-                            else{
-                                print("PIN Desconocido")
-                            }
-                        }) {
-                            Text("Unirse a la Sesión")
-                                .frame(width: 200, height: 50)
-                                .background(Color(red: 255/255, green: 87/255, blue: 50/255))
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .bold()
-                        }
-                        .padding()
-                        
-                        NavigationLink(
-                            destination: LiveSession(viewModel: LiveSessionViewModel()),
-                            isActive: $isSessionJoined,
-                            label: {
-                                EmptyView()
-                            }
-                        )
-                    } else if showQRScanner {
-                        // Vista para escanear el código QR (necesitarás implementarlo utilizando UIKit)
-                        QRScannerView()
-                    }
-                    Spacer()
-                }
+                Text("JOIN SESSION")
+                    .font(.system(size: 40))  // Aumenta el tamaño de la fuente
+                    .padding()  // Añade un poco de espacio alrededor
             }
-            .navigationBarTitle("") // Oculta el título de navegación
-            .navigationBarHidden(true) // Oculta la barra de navegación
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
-
-
-struct SessionView: View {
-    var body: some View {
-        // Contenido de la vista de sesión después de unirse
-        Text("¡Te has unido a la sesión!")
-    }
-}
-struct QRScannerView: View {
-    var body: some View {
-        // Aquí debes implementar la vista que abre la cámara y escanea el código QR utilizando UIKit
-        // Puedes hacerlo en una representación UIViewControllerRepresentable
-        Text("Escanea QR aquí")
-            .font(.largeTitle)
-            .onAppear {
-                // Aquí debes abrir la cámara para escanear el código QR
-            }
-    }
-}
-
 
 struct JoinSession_Preview: PreviewProvider {
     static var previews: some View {
         JoinSessionView()
     }
 }
+
