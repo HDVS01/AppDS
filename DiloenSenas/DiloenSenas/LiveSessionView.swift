264a3f54ca8dfc7dfedda9607df18fc274e40db7
Juanster08 <88806110+Juanster08@users.noreply.github.com>
2023-10-18 19:38:09 -0600

    VIDEO

--- DiloenSenas/DiloenSenas/LiveSessionView.swift
+++ DiloenSenas/DiloenSenas/LiveSessionView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct LiveSession: View {
     @State private var isPresented: Bool = false
@@ -15,9 +16,12 @@ struct LiveSession: View {
     @ObservedObject var viewModel: LiveSessionViewModel // Cambia a usar el ViewModel
     @State private var isShowingAlert = false
     @State private var alertMessage = ""
-        
-
+    
+    
     var body: some View {
+        
+        let url = URL(string:"https://youtu.be/hDRTRgXGklU")!
+        
         GeometryReader { geo in
             ZStack {
                 Image("fondosolo")
@@ -25,129 +29,124 @@ struct LiveSession: View {
                     .scaledToFill()
                     .frame(width: geo.size.width)
                     .edgesIgnoringSafeArea(.all)
+                
                 VStack {
-                    ZStack(alignment: .bottom) {
+                    ZStack{
                         Rectangle()
                             .foregroundColor(Color(red: 55/255, green: 215/255, blue: 70/255))
                             .cornerRadius(10)
                             .frame(width: 550, height: 350)
                         
-                        VStack{
-                            Image(systemName: "video") // aquí va la imagen o el video
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 190) // Tamaño de la imagen/video
-                            Text("Instrucciones")
-                                .font(.headline)
-                                .padding()
-                        }
+                        WebView(urlString: "https://www.youtube.com/embed/hDRTRgXGklU")
+                            .frame(width: 500, height: 300)
                     }
-                    
-                    
-                    VStack {
-                        Rectangle()
-                            .strokeBorder(Color(red: 55/255, green: 215/255, blue: 70/255),style: StrokeStyle(lineWidth: 5))
-                            .overlay(
-                                Group {
-                                    if let selectedImage = selectedImage {
-                                        Image(uiImage: selectedImage)
-                                            .resizable()
-                                            .scaledToFit()
-                                    }
-                                }
-                            )
                         
                         VStack {
-                            //print(viewModel.objectsToScan[viewModel.currentObjectIndex].name)
-                            Button(action: {
+                            Rectangle()
+                                .strokeBorder(Color(red: 55/255, green: 215/255, blue: 70/255),style: StrokeStyle(lineWidth: 5))
+                                .overlay(
+                                    Group {
+                                        if let selectedImage = selectedImage {
+                                            Image(uiImage: selectedImage)
+                                                .resizable()
+                                                .scaledToFit()
+                                        }
+                                    }
+                                )
+                            
+                            VStack {
+                                //print(viewModel.objectsToScan[viewModel.currentObjectIndex].name)
+                                Button(action: {
+                                    
+                                    let userWon = true
+                                    let userLose = true
+                                    
+                                    if userWon {
+                                        alertMessage = "¡Ganaste!"
+                                    } else {
+                                        alertMessage = "Perdiste."
+                                    }
+                                    
+                                    // Mostrar el pop-up.
+                                    isShowingAlert = true
+                                    
+                                    if let selectedImage = selectedImage {
+                                        /*
+                                         if viewModel.imageClassifier.detect(uiImage: selectedImage) == viewModel.objectsToScan[viewModel.currentObjectIndex].name {
+                                         viewModel.objectDetectedSuccessfully()
+                                         }
+                                         */
+                                        viewModel.imageClassifier.detect(uiImage: selectedImage)
+                                        if viewModel.imageClassifier.imageClass == viewModel.objectsToScan[viewModel.currentObjectIndex].name {
+                                            viewModel.objectDetectedSuccessfully()
+                                        }
+                                        print("-----------------------------------------------------")
+                                        print(viewModel.objectsToScan[viewModel.currentObjectIndex].name)
+                                        print("-----------------------------------------------------")
+                                        
+                                    }
+                                }) {
+                                    Image(systemName: "camera.fill")
+                                        .foregroundColor(.green)
+                                        .font(.title)
+                                }
                                 
-                                let userWon = true
-                                let userLose = true
-
-                                if userWon {
-                                    alertMessage = "¡Ganaste!"
-                                } else {
-                                    alertMessage = "Perdiste."
+                                .alert(isPresented: $isShowingAlert) {
+                                    Alert(title: Text("Resultado"), message: Text(alertMessage), dismissButton: .default(Text("Aceptar")))
                                 }
-
-                                // Mostrar el pop-up.
-                                isShowingAlert = true
-                
-                                if let selectedImage = selectedImage {
-                                    /*
-                                     if viewModel.imageClassifier.detect(uiImage: selectedImage) == viewModel.objectsToScan[viewModel.currentObjectIndex].name {
-                                     viewModel.objectDetectedSuccessfully()
-                                     }
-                                     */
-                                    viewModel.imageClassifier.detect(uiImage: selectedImage)
-                                    if viewModel.imageClassifier.imageClass == viewModel.objectsToScan[viewModel.currentObjectIndex].name {
-                                        viewModel.objectDetectedSuccessfully()
+                                
+                                if let imageClass = viewModel.imageClassifier.imageClass {
+                                    HStack {
+                                        Text("Categorías de la imagen:")
+                                            .font(.caption)
+                                        Text(imageClass)
+                                            .bold()
+                                        
                                     }
-                                    print("-----------------------------------------------------")
-                                    print(viewModel.objectsToScan[viewModel.currentObjectIndex].name)
-                                    print("-----------------------------------------------------")
-                                    
-                                }
-                            }) {
-                                Image(systemName: "camera.fill")
-                                    .foregroundColor(.green)
-                                    .font(.title)
-                            }
-                            
-                            .alert(isPresented: $isShowingAlert) {
-                                        Alert(title: Text("Resultado"), message: Text(alertMessage), dismissButton: .default(Text("Aceptar")))
-                                    }
-                            
-                            if let imageClass = viewModel.imageClassifier.imageClass {
-                                HStack {
-                                    Text("Categorías de la imagen:")
-                                        .font(.caption)
-                                    Text(imageClass)
-                                        .bold()
-                                    
-                                }
-                                .font(.subheadline)
-                                .padding()
-                            } else {
-                                Text("Categorías de la imagen: NA")
-                                    .font(.caption)
+                                    .font(.subheadline)
                                     .padding()
-                            }
-                        }
-                        .padding()
-                    }
-                    .sheet(isPresented: $isPresented) {
-                        ImagePickerView(selectedImage: $selectedImage, isPresented: $isPresented, sourceType: $sourceType)
-                            .onDisappear {
-                                if let selectedImage = selectedImage {
-                                    viewModel.imageClassifier.detect(uiImage: selectedImage)
+                                } else {
+                                    Text("Categorías de la imagen: NA")
+                                        .font(.caption)
+                                        .padding()
                                 }
                             }
-                    }
-                    .padding()
-                    
-                    HStack {
-                        Button(action: {
-                            isPresented = true
-                            sourceType = .camera
-                            print("-----------------------------------------------------")
-                            print(viewModel.objectsToScan[viewModel.currentObjectIndex].name)
-                            print("-----------------------------------------------------")
-                        }) {
-                            Text("Escanear")
-                                .frame(width: 150, height: 50)
-                                .background(Color(red: 55/255, green: 215/255, blue: 70/255))
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .font(Font.custom("Rowdies-Regular", size: 20))
+                            .padding()
+                        }
+                        .sheet(isPresented: $isPresented) {
+                            ImagePickerView(selectedImage: $selectedImage, isPresented: $isPresented, sourceType: $sourceType)
+                                .onDisappear {
+                                    if let selectedImage = selectedImage {
+                                        viewModel.imageClassifier.detect(uiImage: selectedImage)
+                                    }
+                                }
                         }
                         .padding()
+                        
+                        HStack {
+                            Button(action: {
+                                isPresented = true
+                                sourceType = .camera
+                                print("-----------------------------------------------------")
+                                print(viewModel.objectsToScan[viewModel.currentObjectIndex].name)
+                                print("-----------------------------------------------------")
+                            }) {
+                                Text("Escanear")
+                                    .frame(width: 150, height: 50)
+                                    .background(Color(red: 55/255, green: 215/255, blue: 70/255))
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                                    .font(Font.custom("Rowdies-Regular", size: 20))
+                            }
+                            .padding()
+                        }
                     }
                 }
             }
         }
     }
-}
+
+
 
 struct LiveSession_Previews: PreviewProvider {
     static var previews: some View {
---
+++ DiloenSenas/DiloenSenas/WebKitView.swift
@@ -0,0 +1,41 @@
+//
+//  WebKitView.swift
+//  DiloenSenas
+//
+//  Created by Alicia Ayón on 18/10/23.
+//
+
+import SwiftUI
+import WebKit
+
+
+struct WebView: UIViewRepresentable {
+    let urlString: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebView
+        
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        // Implementa métodos de WKNavigationDelegate si es necesario
+    }
+}
